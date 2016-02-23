bf68d99988c1d995e541421f8f3bb3bb86a9ce8e
Dom England <dom@raininglemons.com>
2016-02-23 22:15:15 +0000

    Works as additional plugin with es2015 preset now, no need for duplicate code

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    es2015Angular = require('./es2015-angular.js').plugins;
+    babel = require('gulp-babel');
 
 gulp.task('default', () =>
     gulp.src('test.js')
         .pipe(babel({
-            plugins: es2015Angular
+            presets: ["es2015"],
+            plugins: [
+                require("./index.js")
+            ]
         }))
         .pipe(gulp.dest('dist'))
 );
