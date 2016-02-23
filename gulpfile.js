9835d70697721ac516ecb1fe04113cfdd6e464a3
Dom England <dom@raininglemons.com>
2016-02-23 01:38:35 +0000

    Because unicorns...

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ const gulp = require('gulp'),
     babel = require('gulp-babel'),
     es2015Angular = require('./es2015-angular.js').plugins;
 
-gulp.task('default', () => {
-    return gulp.src('test.js')
+gulp.task('default', () =>
+    gulp.src('test.js')
         .pipe(babel({
             plugins: es2015Angular
         }))
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(gulp.dest('dist'))
+);
